9fab59d9865c23f6d794fef801256041db0a62e2
Samet Akca <+akcasamet99@users.noreply.github.com>
2021-10-19 00:03:15 +0300

    JS Kampı İLK DERS SONU

---
+++ src/app.js
@@ -0,0 +1,39 @@
+console.log("Hello World")
+
+// JS type safe değildir.
+
+let  USDToday = 3.20
+let  USDYesterday = 3.10
+
+
+console.log("USD BUGÜN : "  + USDToday)
+console.log("USD Dün : "  + USDYesterday)
+
+if(USDToday > USDYesterday){
+  console.log("Dolar artdı img")
+    }
+    else if (USDToday < USDYesterday) {
+        console.log("Dolar düştü img")
+
+    } else {
+        
+        console.log("Dolar aynı img")
+    }
+
+    const EuroDun = 11.20
+    
+    console.log( "Euro Dun : "  + EuroDun)
+
+    // Array
+    // camelCasing
+    // PascalCasing
+
+    
+    let konutKredileri = ["A Kredisi" ," B Kredisi" , " C Kredisi "]
+    
+    for (let i =0 ;  i < konutKredileri.length; i++) {
+
+        console.log(konutKredileri[i])
+
+        }
+    
